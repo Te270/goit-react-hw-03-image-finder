f3402bf64135cc6dfe2a559a42189ad3c5c8959d
Te270 <79935865+Te270@users.noreply.github.com>
2021-11-18 17:53:49 +0200

    Черновой вариант

---
+++ src/services/imagesApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const apiKey = '24374609-133688d7da004cfa06346cc34';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const fetchData = async ({ searchQuery = '', currentPage = 1 }) => {
+  const response = await axios.get(
+    `/?q=${searchQuery}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`,
+  );
+  return response.data.hits;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { fetchData };
